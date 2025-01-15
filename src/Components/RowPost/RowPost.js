98670cf00b9f0a87893bf0f851fb43d6eeb7d7ce
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-01-15 21:11:01 +0530

    Improved some ui and integrated api call for loading trailors of movies on click

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,10 +1,12 @@
 import React,{useEffect,useState} from 'react'
 import './RowPost.css'
 import axios from '../../Axios'
-import {imageUrl} from '../../Constants/Constants'
+import {imageUrl,API_KEY} from '../../Constants/Constants'
+import Youtube from 'react-youtube'
 
 function RowPost(props) {
   const [movies, setMovies] = useState([])
+  const [urlId,setUrlId] = useState('')
   useEffect(()=>{
     axios.get(props.url).then(response=>{
       console.log(response.data)
@@ -12,15 +14,33 @@ function RowPost(props) {
     }).catch(err=>{
       alert('Network error')
     })
-  },[])
+  },[props.url])
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleMovie = (id)=>{
+    console.log(id)
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
+      if(response.data.results.length!==0){
+        setUrlId(response.data.results[0])
+      }else{
+        console.log('No items to show')
+      }
+    })
+  }
   return (
     <div className='row'>
       <h2>{props.title}</h2>
       <div className="posters">
         {movies.map((obj)=>
-          <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt="Poster" />
+          <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt="Poster" />
         )}
       </div>
+      {urlId && <Youtube opts={opts} videoId={urlId.key} />}
     </div>
   )
 }
